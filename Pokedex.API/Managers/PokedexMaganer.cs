2255d4df4180e465a6ab3fd0d26118bbda84638e
Vagabundo <robertodelrior@gmail.com>
2021-12-11 10:36:21 +0000

    Create data and services/managers structure

---
+++ Pokedex.API/Managers/PokedexMaganer.cs
@@ -0,0 +1,11 @@
+using Pokedex.API.Clients;
+
+namespace Pokedex.API.Managers
+{
+    public class PokedexManager {
+        public PokedexManager(IPokemonClient pokemonClient)
+        {
+            // Further functions here
+        }
+    }
+}
